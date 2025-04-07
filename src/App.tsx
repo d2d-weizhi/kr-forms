dd87766c3eec80237d3f26a5b8e08461d39178c8
d2d.weizhi <d2d.weizhi@gmail.com>
2025-04-07 17:07:25 +0800

    Recreated the project. Still can't get tailwindcss to work.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import '@progress/kendo-theme-default/dist/all.css';
-import { Calendar } from '@progress/kendo-react-dateinputs';
-import './App.css';
+import { Button } from '@progress/kendo-react-buttons';
+import kendoka from './kendoka.svg';
 
 function App() {
+  const handleClick = React.useCallback(() => {
+    window.open('https://www.telerik.com/kendo-react-ui/components/', '_blank');
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello KendoReact!</h1>
-      <Calendar />
+      <div className="App-header">
+        <img src={kendoka} className="App-logo" alt="kendoka" />
+        <div className="text-content k-rounded-lg">
+          Edit <code>src/App.tsx</code> and save to reload.
+        </div>
+        <Button
+          themeColor={'primary'}
+          size={"large"}
+          onClick={handleClick}
+        >
+          Learn KendoReact
+        </Button>
+      </div>
     </div>
   );
 }
