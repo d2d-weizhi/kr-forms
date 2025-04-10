3cb0319de247e01e480a21c7e60b129a8ac222a4
d2d.weizhi <d2d.weizhi@gmail.com>
2025-04-10 17:58:59 +0800

    Managed to add a few more form components today.

--- src/components/shared/form-components.tsx
+++ src/components/shared/form-components.tsx
@@ -26,6 +26,7 @@ import {
   DropDownTree
 } from '@progress/kendo-react-dropdowns';
 import { processTreeData, expandedState } from './tree-data-operations';
+import { AnyARecord } from 'node:dns';
 
 interface columnsInterface {
 	field: string;
@@ -305,4 +306,245 @@ export const FormColorPicker = (fieldRenderProps: FieldRenderProps) => {
 	);
 };
 
-// Continue from FormSlider next...
+export const FormSlider = (fieldRenderProps: FieldRenderProps) => {
+	const { validationMessage, touched, label, id, valid, disabled, hint, data, min, max, ...others } = fieldRenderProps;
+	const editorRef = useRef<any>(null);
+
+	const showValidationMessage: string | false | null = touched && validationMessage;
+	const showHint: boolean = !showValidationMessage && hint;
+	const hintId: string = showHint ? `${id}_hint` : '';
+	const errorId: string = showValidationMessage ? `${id}_error` : '';
+	const labelId: string = label ? `${id}_label` : '';
+
+	return (
+		<FieldWrapper>
+			<Label 
+				id={labelId}
+				editorRef={editorRef}
+				editorId={id}
+				editorValid={valid}
+				editorDisabled={disabled}
+				className="k-form-label"
+			>
+				{label}
+			</Label>
+			<div className={'k-form-field-wrap'}>
+				<Slider 
+					ariaLabelledBy={labelId}
+					ariaDescribedBy={`${hintId} ${errorId}`}
+					ref={editorRef}
+					valid={valid}
+					id={id}
+					disabled={disabled}
+					min={min}
+					max={max}
+					{...others}
+				>
+					{data.map((value: any) =>(
+						<SliderLabel title={value} key={value} position={value}>
+							{value.toString()}
+						</SliderLabel>
+					))}
+				</Slider>
+				{showHint && <Hint id={hintId}>{hint}</Hint>}
+				{showValidationMessage && <Error id={errorId}>{validationMessage}</Error>}
+			</div>
+		</FieldWrapper>
+	);
+};
+
+export const FormRangeSlider = (fieldRenderProps: FieldRenderProps) => {
+	const { validationMessage, touched, label, id, valid, hint, disabled, data, min, max, ...others } = fieldRenderProps;
+
+	const showValidationMessage: string | false | null = touched && validationMessage;
+	const showHint: boolean = !showValidationMessage && hint;
+	const hintId: string = showHint ? `${id}_hint` : '';
+	const errorId: string = showValidationMessage ? `${id}_error` : '';
+
+	return (
+		<FieldWrapper>
+			<Label editorId={id} editorValid={valid} className="k-form-label">
+				{label}
+			</Label>
+			<div className={'k-form-field-wrap'}>
+				<RangeSlider
+					valid={valid}
+					id={id}
+					disabled={disabled}
+					ariaDescribedBy={`${hintId} ${errorId}`}
+					min={min}
+					max={max}
+					{...others}
+				>
+					{data.map((value: any) => {
+						return (
+							<SliderLabel key={value} position={value}>
+								{value.toString()}
+							</SliderLabel>
+						);
+					})}
+				</RangeSlider>
+				{showValidationMessage && <Error id={errorId}>{validationMessage}</Error>}
+			</div>
+		</FieldWrapper>
+	);
+};
+
+export const FormRating = (fieldRenderProps: FieldRenderProps) => {
+	const { validationMessage, touched, label, id, valid, hint, disabled, optional, ...others } = fieldRenderProps;
+
+	const showValidationMessage: string | false | null = touched && validationMessage;
+	const showHint: boolean = !showValidationMessage && hint;
+	const hintId: string = showHint ? `${id}_hint` : '';
+	const errorId: string = showValidationMessage ? `${id}_error` : '';
+
+	return (
+		 <FieldWrapper>
+			<Label
+				editorId={id}
+				editorValid={valid}
+				optional={optional}
+				className="k-form-label"
+			>
+				{label}
+			</Label>
+			<div className={'k-form-field-wrap'}>
+				<Rating 
+					valid={valid}
+					id={id}
+					disabled={disabled}
+					ariaDescribedBy={`${hintId} ${errorId}`}
+					{...others}
+				/>
+				{showValidationMessage && <Error id={errorId}>{validationMessage}</Error>}
+			</div>
+		 </FieldWrapper>
+	);
+};
+
+export const FormUpload = (fieldRenderProps: FieldRenderProps) => {
+	const { value, id, optional, label, hint, validationMessage, touched, ...others } = fieldRenderProps;
+
+	const showValidationMessage: string | false | null = touched && validationMessage;
+	const showHint: boolean = !showValidationMessage && hint;
+	const hintId: string = showHint ? `${id}_hint` : '';
+	const errorId: string = showValidationMessage ? `${id}_error` : '';
+	const labelId: string = label ? `${id}_label` : '';
+
+	const onChangeHandler = (event: any) => {
+		fieldRenderProps.onChange({ value: event.newState });
+	};
+
+	const onRemoveHandler = (event: any) => {
+		fieldRenderProps.onChange({ value: event.newState });
+	};
+
+	return (
+		<FieldWrapper>
+			<Label
+				id={labelId}
+				editorId={id}
+				optional={optional}
+				className="k-form-label"
+			>
+				{label}
+			</Label>
+			<div className={'k-form-field-wrap'}>
+				<Upload 
+					id={id}
+					autoUpload={false}
+					showActionButtons={false}
+					multiple={false}
+					files={value}
+					onAdd={onChangeHandler}
+					onRemove={onChangeHandler}
+					ariaDescribedBy={`${hintId} ${errorId}`}
+					ariaLabelledBy={labelId}
+					{...others}
+				/>
+				{showHint && <Hint id={hintId}>{hint}</Hint>}
+				{showValidationMessage && <Error id={errorId}>{validationMessage}</Error>}
+			</div>
+		</FieldWrapper>
+	);
+};
+
+export const FormDropDownList = (fieldRenderProps: FieldRenderProps) => {
+	const { validationMessage, touched, label, id, valid, disabled, hint, wrapperStyle, ...others } = fieldRenderProps;
+	const editorRef = useRef<any>(null);
+
+	const showValidationMessage: string | false | null = touched && validationMessage;
+	const showHint: boolean = !showValidationMessage && hint;
+	const hintId: string = showHint ? `${id}_hint` : '';
+	const errorId: string = showValidationMessage ? `${id}_error` : '';
+	const labelId: string = label ? `${id}_label` : '';
+
+	return (
+		<FieldWrapper style={wrapperStyle}>
+			<Label
+				id={labelId}
+				editorRef={editorRef}
+				editorId={id}
+				editorValid={valid}
+				editorDisabled={disabled}
+				className="k-form-label"
+			>
+				{label}
+			</Label>
+			<div className={'k-form-field-wrap'}>
+				<DropDownList 
+					ariaLabelledBy={labelId}
+					ariaDescribedBy={`${hintId} ${errorId}`}
+					ref={editorRef}
+					valid={valid}
+					id={id}
+					disabled={disabled}
+					{...others}
+				/>
+				{showHint && <Hint id={hintId}>{hint}</Hint>}
+				{showValidationMessage && <Error id={errorId}>{validationMessage}</Error>}
+			</div>
+		</FieldWrapper>
+	);
+};
+
+export const FormAutoComplete = (fieldRenderProps: FieldRenderProps) => {
+	const { validationMessage, touched, label, id, valid, disabled, hint, wrapperStyle, ...others } = fieldRenderProps;
+	const editorRef = useRef<any>(null);
+
+	const showValidationMessage: string | false | null = touched && validationMessage;
+	const showHint: boolean = !showValidationMessage && hint;
+	const hintId: string = showHint ? `${id}_hint` : '';
+	const errorId: string = showValidationMessage ? `${id}_error` : '';
+	const labelId: string = label ? `${id}_label` : '';
+
+	return (
+		<FieldWrapper style={wrapperStyle}>
+			<Label
+				id={labelId}
+				editorRef={editorRef}
+				editorId={id}
+				editorValid={valid}
+				editorDisabled={disabled}
+				className="k-form-label"
+			>
+				{label}
+			</Label>
+			<div className={'k-form-field-wrap'}>
+				<AutoComplete 
+					ariaLabelledBy={labelId}
+					ariaDescribedBy={`${hintId} ${errorId}`}
+					ref={editorRef}
+					valid={valid}
+					id={id}
+					disabled={disabled}
+					{...others}
+				/>
+				{showHint && <Hint id={hintId}>{hint}</Hint>}
+				{showValidationMessage && <Error id={errorId}>{validationMessage}</Error>}
+			</div>
+		</FieldWrapper>
+	);
+};
+
+// Continue from ComboBox
